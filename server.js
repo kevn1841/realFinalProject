f88d9385af992a48b3a37c599931737e938ca61d
Kevin Shukla <kevn1841@gmail.com>
2016-10-11 19:08:28 -0400

    A

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require("path");
 
 var app = express();
+var PORT = process.env.PORT || 3000;
 //Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
+app.get('/form', function(req, res){
+	res.render('form.html')
+});
 
-var port = 3000;
-app.listen(port);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:"application/vnd.api+json"}));
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT " + PORT);
+});
+
 
 console.log(module.exports)
