aa5f6397e25a79f092fdd0901b53c0cbc780d5d6
Kevin Shukla <kevn1841@gmail.com>
2016-10-15 10:59:17 -0400

    nicer code

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var bodyParser = require('body-parser');
 var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
-//Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/public/views');
 app.engine('html', require('ejs').renderFile);
